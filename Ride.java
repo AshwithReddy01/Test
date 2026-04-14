2489d7c3c4d37a298c73cb9b0bfb9c42a3bbaf75
AshwithReddy01 <ashwithreddy01@gmail.com>
2026-04-14 15:42:22 +0530

    Ride.java

--- Ride.java
+++ Ride.java
@@ -1,33 +1,23 @@
 package Test;
 
 abstract class Ride {
-
-    // 🔒 Private fields (Encapsulation)
     private int rideId;
     private double distance;
     private double baseFare;
-
-    // ✅ Constructor
     Ride(int rideId, double distance, double baseFare) {
         this.rideId = rideId;
-        setDistance(distance);   // validation
-        setBaseFare(baseFare);   // validation
+        setDistance(distance);   
+        setBaseFare(baseFare);   
     }
-
-    // 🔹 Getters
     public int getRideId() {
         return rideId;
     }
-
     public double getDistance() {
         return distance;
     }
-
     public double getBaseFare() {
         return baseFare;
     }
-
-    // 🔹 Setters with Validation
     public void setDistance(double distance) {
         if (distance > 0) {
             this.distance = distance;
@@ -43,11 +33,7 @@ abstract class Ride {
             System.out.println("Base fare must be >= 0");
         }
     }
-
-    // 🔸 Abstract Method
     abstract double calculateFare();
-
-    // 🔸 Common Method
     void displayRideDetails() {
         System.out.println("Ride ID: " + rideId);
         System.out.println("Distance: " + distance + " km");
