c01d960d60ae9a70bb133d9187c961fdd1032a4d
AshwithReddy01 <ashwithreddy01@gmail.com>
2026-04-14 15:42:54 +0530

    RideService.java

--- RideService.java
+++ RideService.java
@@ -1,19 +1,13 @@
 package Test;
 
 class RideService {
-
-    // 🔹 Calculate total revenue
     double calculateTotalRevenue(Ride[] rides) {
         double total = 0;
-
         for (Ride r : rides) {
             total += r.calculateFare();
         }
-
         return total;
     }
-
-    // 🔹 Find ride with highest fare
     Ride findHighestFareRide(Ride[] rides) {
         Ride maxRide = rides[0];
 
