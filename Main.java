4c5b78bfb089564ba851d9c52a475ba63e1699ac
AshwithReddy01 <ashwithreddy01@gmail.com>
2026-04-14 15:43:26 +0530

    Main.java

--- Main.java
+++ Main.java
@@ -2,16 +2,10 @@ package Test;
 
 public class Main {
     public static void main(String[] args) {
-
-        // Create array of Ride (Polymorphism)
         Ride[] rides = new Ride[3];
-
-        // Store different ride objects
         rides[0] = new MiniRide(101, 5, 50);
         rides[1] = new PrimeRide(102, 10, 50);
         rides[2] = new PoolRide(103, 8, 50);
-
-        // Loop and call methods dynamically
         for (Ride r : rides) {
             r.displayRideDetails();
             System.out.println("Total Fare: " + r.calculateFare());
